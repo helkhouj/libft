4d1e151c576bc438938aa0304f985130a55c4c02
Hicham Elkhouji <helkhouj@e2r9p10.1337.ma>
2024-10-22 13:34:03 +0100

    some basic functions that are included in the ctype library

---
+++ libft.h
@@ -0,0 +1,20 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+// Part 1 - Libc functions
+
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+size_t ft_strlen(const char *s);
+
+
+
+#endif 
+ 
